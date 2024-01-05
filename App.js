bba3301cf7ad2dca7bbaaa4e0c31fadf231b4b0f
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-01-05 13:48:29 -0300

    ajustes no layout

--- App.js
+++ App.js
@@ -1,24 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 const App = () => {
   const [registros, setRegistros] = useState([]);
 
   useEffect(() => {
+    // Sua lógica para buscar registros do servidor
     fetch('http://localhost:3000/listarRegistros')
       .then(response => response.json())
       .then(data => setRegistros(data))
       .catch(error => console.error('Erro ao buscar dados:', error));
   }, []);
 
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.text}>{item.FerramentaID}</Text>
+      <Text style={styles.text}>{item.Nome}</Text>
+      <Text style={styles.text}>{item.QuantidadeDisponivel}</Text>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>Lista de Ferramentas</Text>
-      {registros && registros.map(registro => (
-        <Text key={registro.FerramentaID}><b>Cód. : </b>{registro.FerramentaID} - <b>Nome: </b>{registro.Nome}</Text>
-      ))}
+    <View style={styles.container}>
+      <FlatList
+        data={registros}
+        keyExtractor={(item) => item.FerramentaID.toString()}
+        renderItem={renderItem}
+        ListHeaderComponent={() => (
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Cód.</Text>
+            <Text style={styles.headerText}>Nome</Text>
+            <Text style={styles.headerText}>Qtd. Disponível</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    paddingTop: 40,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    margin: 2,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  text: {
+    fontSize: 18,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: '#333',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
